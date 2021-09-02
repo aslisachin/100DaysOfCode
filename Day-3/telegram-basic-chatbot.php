ea3499eb1cfe9dbfe7b488d6f4638398fa7effe9
Sachin Sharma <sachinmodgil6@gmail.com>
2021-09-02 10:22:23 +0530

    Update telegram-basic-chatbot.php

--- Day-3/telegram-basic-chatbot.php
+++ Day-3/telegram-basic-chatbot.php
@@ -9,11 +9,23 @@ $token="YOUR BOT TOKEN";
 if($text=="Hello"){
     $message = "Hello There! $name";
 }
+elseif($text=="Hi"){
+    $message = "Hello";
+}
+elseif($text=="My name"){
+    $message = "$name";
+}
+elseif($text=="random number"){
+    $message = rand(1,10000);
+}
+elseif($text=="I love you"){
+    $message = "I Love Uou Too $name";
+}
 elseif($text=="What is your name"){
     $message = "My Name Is Test Bot";
 }
 else{
-    $message = "Sorry I Can't Understand";
+    $message = "Sorry I Can't Understand Your Message ($text)";
 }
 
 $url="https://api.telegram.org/bot$token/sendMessage?text=$message&chat_id=$chat_id";
